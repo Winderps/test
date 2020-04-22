52a57f7597376bdc51282a0fdad80cd30a95f89a
Winderps <homerun32295@gmail.com>
2020-04-22 18:08:54 -0400

    Initial Commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("buzz");
+                }
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write(i);
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
